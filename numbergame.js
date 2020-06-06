394384ef08636579b56318adbda2b48c0d3b72ae
dawndarkness <62203696+dawndarkness@users.noreply.github.com>
2020-06-06 23:42:30 +0900

    Create numbergame.js

---
+++ numbergame.js
@@ -0,0 +1,16 @@
+function generate(number) {
+   var number =  Math.floor((Math.random() * 500) + 1);
+}
+
+var userGuess = window.prompt("What is the number?: ");
+
+function userGuess() {
+    while (userGuess != number) {
+        if (userGuess < number) alert ("higher");
+        var userGuess = window.prompt("What is your next guess?: ");
+        if (userGuess > number) return ("lower");
+        var userGuess = window.prompt("Can you get any hotter?: ");
+        break;
+    if (userGuess === number) alert ("Congratulations! You have successfully guessed the number");
+    }
+}
